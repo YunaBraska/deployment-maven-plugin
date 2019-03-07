5ac2341a66453574435439d058f7d94fccaf814b
yuna90 <io@yuna.berlin>
2019-03-07 22:43:25 +0100

    added testing

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 This is an example how to separate the deployment from build process in maven which I am using for my deployments to keep also the pom.xml small and not have a parent which is needed to be also in maven central 
 
 ### TODO
+* [ ] Maven custom meta data like UTF-8, Java version, custom param....
+* [ ] SCM DEPLOY TAG
+* [ ] org.sonatype.plugins
+* [ ] RELEASE <autoVersionSubmodules>true</autoVersionSubmodules> <useReleaseProfile>true</useReleaseProfile>
 * [ ] own or buy logo https://www.designevo.com/apps/logo/?name=blue-hexagon-and-3d-container
 
 ![command-line-util](src/main/resources/banner.png "command-line-util")
--- ci.bash
+++ ci.bash
@@ -7,33 +7,58 @@ MVN_PROFILES="true"
 GPG_PASSPHRASE=''
 PROJECT_VERSION=''
 IS_POM='false'
+MVN_CLEAN='true'
+MVN_UPDATE='true'
+MVN_JAVA_DOC='true'
+MVN_SOURCE='true'
 
 MVN_CLEAN_CMD='clean verify'
 MVN_CLEAN_PARAM='generate-resources generate-sources dependency:resolve-plugins'
-
-MVN_CACHE_CMD='dependency:purge-local-repository'
+MVN_UPDATE_CMD="versions:update-parent versions:update-properties versions:update-child-modules versions:use-latest-releases versions:use-next-snapshots versions:commit -DallowSnapshots=true -DgenerateBackupPoms=false"
+MVN_DOC_CMD="javadoc:jar"
+MVN_DOC_PARAM="-Dnodeprecatedlist -Dquiet=true"
+MVN_SOURCE_CMD="source:jar-no-fork"
+MVN_GPG_CMD="gpg:sign"
+MVN_GPG_PARAM="-Darguments=-D--pinentry-mode -Darguments=loopback"
 
 for i in "$@"
 do
 case $i in
     -V=*|--PROJECT_VERSION=*)
     PROJECT_VERSION="${i#*=}"
+    MVN_UPDATE_CMD="versions:set -DnewVersion=${PROJECT_VERSION} ${MVN_UPDATE_CMD}"
     shift # past argument=value
     ;;
-    -P=*|--MVN_PROFILES=*)
-    MVN_PROFILES="${i#*=}"
+    -C=*|--MVN_CLEAN=*)
+    MVN_CLEAN="${i#*=}"
     shift # past argument=value
     ;;
-    -G=*|--GPG_PASSPHRASE=*)
-    GPG_PASSPHRASE="${i#*=}"
+    -D=*|--MVN_JAVA_DOC=*)
+    MVN_JAVA_DOC="${i#*=}"
     shift # past argument=value
     ;;
     -E=*|--ENCODING=*)
     ENCODING="${i#*=}"
     shift # past argument=value
     ;;
+    -G=*|--GPG_PASSPHRASE=*)
+    GPG_PASSPHRASE="${i#*=}"
+    shift # past argument=value
+    ;;
     -J=*|--JAVA_VERSION=*)
     JAVA_VERSION="${i#*=}"
+    shift # past argument=value
+    ;;
+    -P=*|--MVN_PROFILES=*)
+    MVN_PROFILES="${i#*=}"
+    shift # past argument=value
+    ;;
+    -S=*|--MVN_SOURCE=*)
+    MVN_SOURCE="${i#*=}"
+    shift # past argument=value
+    ;;
+    -U=*|--MVN_UPDATE=*)
+    MVN_UPDATE="${i#*=}"
     shift # past argument=value
     ;;
     --default)
@@ -55,95 +80,34 @@ else
   MVN_PROFILES=""
 fi
 
-#if [ ${MVN_PROFILES} = "true" ]; then
-#fi
-
 echo "########## SETUP ENVIRONMENT ##########"
+if [ ${MVN_UPDATE} = "false" ]; then MVN_UPDATE_CMD=''; else MVN_UPDATE='true'; fi
+if [ ${MVN_CLEAN} = "false" ]; then MVN_CLEAN_CMD=''; else MVN_UPDATE='true'; MVN_CLEAN_CMD="dependency:purge-local-repository ${MVN_CLEAN_CMD}"; fi
+if [ ${MVN_JAVA_DOC} = "false" ]; then MVN_DOC_CMD=''; MVN_DOC_PARAM=''; else MVN_JAVA_DOC='true'; fi
+if [ ${MVN_SOURCE} = "false" ]; then MVN_SOURCE_CMD=''; else MVN_SOURCE='true'; fi
+if [ ${#GPG_PASSPHRASE} -ge 2 ]; then MVN_GPG_PARAM="-Dgpg.passphrase=${GPG_PASSPHRASE} ${MVN_GPG_PARAM}"; else MVN_GPG_CMD=''; MVN_GPG_PARAM=''; fi
 
-
-echo "MVN_PROFILES [${MVN_PROFILES}]"
-echo "GPG_PASSPHRASE [${GPG_PASSPHRASE}]"
+#echo "GPG_PASSPHRASE [${GPG_PASSPHRASE}]"
 echo "PROJECT_VERSION [${PROJECT_VERSION}]"
 echo "JAVA_VERSION [${JAVA_VERSION}]"
 echo "ENCODING [${ENCODING}]"
+echo "IS_POM [${IS_POM}]"
+echo "MVN_PROFILES [${MVN_PROFILES}]"
+echo "MVN_CLEAN [${MVN_CLEAN}]"
+echo "MVN_UPDATE [${MVN_UPDATE}]"
+echo "MVN_JAVA_DOC [${MVN_JAVA_DOC}]"
+echo "MVN_SOURCE [${MVN_SOURCE}]"
 
-exit
-
-
-#TODO if no ${PROFILES}
-
-MVN_CLEAN_CMD="clean verify"
-MVN_CLEAN_PARAM="generate-resources generate-sources dependency:resolve-plugins" # -P ${PROFILES}
-
-#if purge local mvn cache
-MVN_CHACHE_CMD="dependency:purge-local-repository"
-
-#if set version
-MVN_VER_PARAM="-DnewVersion=${NEW_VERSION}"
-
-#if update dependencies default == true
-MVN_UPDATE_CMD="versions:set"
-MVN_UPDATE_PARAM="${MVN_VER_PARAM} versions:update-parent versions:update-properties versions:update-child-modules versions:use-latest-releases versions:use-next-snapshots versions:commit -DallowSnapshots=true -DgenerateBackupPoms=false"
-
-#if source default == true
-MVN_DOC_CMD="javadoc:jar"
-MVN_DOC_PARAM="-Dnodeprecatedlist -Dquiet=true"
-
-MVN_SRC_CMD="source:jar-no-fork"
-MVN_SRC_PARAM=""
-
-#if sign default == true
-export GPG_TTY=$(tty)
-gpg -k --armor --always-trust --passphrase=${GPG_PASSPHRASE} --pinentry-mode loopback
-#echo "GPG ACTIVATION" > gpg.act; gpg --sign --armor --always-trust --passphrase=${GPG_PASSPHRASE} --pinentry-mode loopback gpg.act; rm gpg.act gpg.act.asc
-MVN_GPG_CMD="gpg:sign"
-MVN_GPG_PARAM="-Dgpg.passphrase=${GPG_PASSPHRASE} -Darguments=-D--pinentry-mode -Darguments=loopback"
-
-#if print update needs
-MVN_PTU_CMD="versions:display-dependency-updates versions:display-plugins-updates"
-
-
-echo "########## PARAMETERS ##########"
-echo "Encoding [${ENDCODING}]"
-echo "Encoding [${JAVA_VERSION}]"
-if [[ $(grep '<packaging>pom</packaging>' pom.xml | wc -l) = *1* ]] ; then IS_POM=true; fi
-echo "Pom Artifact [${IS_POM}]"
-OLD_VERSION=$(mvn help:evaluate -Dexpression=project.version -Dexpression=project.version | grep -v '\[')
-echo "Version [${OLD_VERSION}] -> [${NEW_VERSION}]"
-
-
-
-echo "Profiles [${PROFILES}]"
-
-mvn ${MVN_CHACHE_CMD} ${MVN_CLEAN_CMD} ${MVN_CLEAN_PARAM} ${MVN_UPDATE_CMD} ${MVN_UPDATE_PARAM} ${MVN_DOC_CMD} ${MVN_DOC_PARAM} ${MVN_SRC_CMD} ${MVN_SRC_PARAM} ${MVN_GPG_CMD} ${MVN_GPG_PARAM}
-exit 0
-
-MAVEN_OPTIONS="-Dproject.build.sourceEncoding=${ENDCODING} -Dproject.encoding=${ENDCODING} -Dproject.reporting.outputEncoding=${ENDCODING} -Dmaven.compiler.source=${JAVA_VERSION} -Dmaven.compiler.target=${JAVA_VERSION} --quiet"
-echo ${MAVEN_OPTIONS}
-echo "########## CLEAN REPOSITORY ##########"
-mvn dependency:purge-local-repository clean generate-resources generate-sources dependency:sources dependency:resolve-plugins -P ${PROFILES} ${MAVEN_OPTIONS}
-#mvn dependency:purge-local-repository clean generate-resources generate-sources dependency:sources dependency:resolve-plugins -P ${PROFILES}
-echo "########## UPDATING PROJECT VERSIONS ##########"
-mvn versions:set -DnewVersion=${NEW_VERSION} versions:update-parent versions:update-properties versions:update-child-modules versions:use-latest-releases versions:use-next-snapshots versions:commit -DallowSnapshots=true -DgenerateBackupPoms=false ${MAVEN_OPTIONS}
-echo "########## BUILD ##########"
-mvn clean verify -DskipTests=true ${MAVEN_OPTIONS}
-if [[ $IS_POM = "false" ]]; then
-    echo "########## GENERATE JAVADOC ##########"
-	#https://maven.apache.org/plugins/maven-javadoc-plugin/
-	mvn javadoc:jar -Dnodeprecatedlist -Dquiet=true ${MAVEN_OPTIONS}
-	echo "########## GENERATE SOURCES ##########"
-	#https://maven.apache.org/plugins/maven-source-plugin/
-	mvn source:jar-no-fork ${MAVEN_OPTIONS}
-	echo "########## SIGN ARTIFACTS ##########"
-	export GPG_TTY=$(tty)
-	#for x in target/*.jar; do gpg --sign --armor --always-trust --passphrase=7576Simba --pinentry-mode loopback "${x}"; done
-	mvn package gpg:sign -DskipTests=true -Dgpg.passphrase=${GPG_PASSPHRASE} -Darguments=-D--pinentry-mode -Darguments=loopback
-	for x in target/*.asc; do gpg --verify "${x}"; done
-	#http://maven.apache.org/plugins/maven-gpg-plugin/sign-mojo.html
+if [ ${#GPG_PASSPHRASE} -ge 2 ]; then
+    export GPG_TTY=$(tty)
+    gpg -k
+    #signing dummy so that maven is using right agent without settings XML
+    echo 'gpg.activation' > gpg.act; gpg --sign --armor --always-trust --passphrase=${GPG_PASSPHRASE} --pinentry-mode loopback gpg.act; rm -rf gpg.act gpg.act.asc
+    #for x in target/*.jar; do gpg --sign --armor --always-trust --passphrase=7576Simba --pinentry-mode loopback "${x}"; done
+    #mvn package gpg:sign -Dgpg.passphrase=${GPG_PASSPHRASE} -Darguments=-D--pinentry-mode -Darguments=loopback
+    #for x in target/*.asc; do gpg --verify "${x}"; done
 fi
-echo "########## TODO: SCM DEPLOY TAG \${project.version} <inherited>false</inherited> ##########"
-echo "########## TODO: org.sonatype.plugins ##########"
-echo "########## TODO: RELEASE <autoVersionSubmodules>true</autoVersionSubmodules> <useReleaseProfile>true</useReleaseProfile> ##########"
-#TODO: mvn versions:display-plugin-updates
 
-#TODO OWN BUILD POM - backup normal one, add steps from build pom and switch back to the old pom
+MVN_ALL="${MVN_CLEAN_CMD} ${MVN_CLEAN_PARAM} ${MVN_UPDATE_CMD} ${MVN_DOC_CMD} ${MVN_DOC_PARAM} ${MVN_SOURCE_CMD} ${MVN_GPG_CMD} ${MVN_GPG_PARAM} versions:display-dependency-updates versions:display-plugin-updates"
+echo "[DEBUG] [${MVN_ALL}]"
+mvn ${MVN_ALL}
