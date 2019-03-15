ce3b405395a4fc77e8f6cf92e8f501716602bf96
Yuna Morgenstern <yuna.morgenstern@ratepay.com>
2019-03-15 15:10:13 +0100

    removed unused test code

--- src/test/java/berlin/yuna/mavendeploy/MainMojoComponentTest.java
+++ src/test/java/berlin/yuna/mavendeploy/MainMojoComponentTest.java
@@ -1,34 +1,15 @@
 package berlin.yuna.mavendeploy;
 
-import berlin.yuna.clu.logic.Terminal;
 import berlin.yuna.mavendeploy.plugin.MainMojo;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.FileReader;
 
 import static java.util.Collections.singletonList;
 
-public class CiComponentTest {
+public class MainMojoComponentTest {
 
     private static final File WORK_DIR = new File(System.getProperty("user.dir"));
-    private Model pomXml;
-
-    @Before
-    public void setUp() throws Exception {
-        buildJar();
-        pomXml = new MavenXpp3Reader().read(new FileReader(new File(WORK_DIR, "pom.xml")));
-    }
-
-    @After
-    public void tearDown() {
-        final File artifactFile = new File(WORK_DIR, pomXml.getArtifactId() + "." + pomXml.getPackaging());
-        new Terminal().dir(WORK_DIR).execute("rm -f " + artifactFile.getAbsolutePath());
-    }
 
     @Test
     public void pluginMojo() {
@@ -49,9 +30,5 @@ public class CiComponentTest {
                 + " --MVN_SOURCE=false"
                 + " --MVN_TAG=false"
                 + " --MVN_TAG_BREAK=false";
-    }
-
-    private void buildJar() {
-        new Terminal().dir(WORK_DIR).execute("mvn clean package -Dmaven.test.skip=true");
     }
 }
