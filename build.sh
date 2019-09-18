60af1fc83fc42a5aa70d7070c595340c2228efe0
Yuna <yuna.morgenstern@finreach.com>
2019-09-18 11:07:03 +0200

    travis test

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "CLU cloning"
 git clone https://github.com/YunaBraska/command-line-util.git clu
 echo "CLU installing"
-mvn --file=clu/pom.xml install -DskipTests=true
+mvn --file=clu/pom.xml install -DskipTests=true --quiet
 echo "CLU removing"
 rm -rf clu
 echo "Start testing"
