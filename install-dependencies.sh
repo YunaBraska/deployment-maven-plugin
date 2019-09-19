3e2307a1e0f60cf917c488c0a625530e1efcfab6
Yuna Morgenstern <io@yuna.berlin>
2019-09-19 20:55:30 +0200

    travis test

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Cleaning maven cache"
+mvn dependency:purge-local-repository --quiet
+mvn clean --quiet
+mvn generate-resources --quiet
+mvn dependency:resolve-plugins --quiet
 echo "CLU cloning"
 git clone https://github.com/YunaBraska/command-line-util.git clu
 echo "CLU installing"
