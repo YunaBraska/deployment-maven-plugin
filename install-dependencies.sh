2c8c339740e8ccb6bc29681911979aeb6efff9b6
Yuna Morgenstern <io@yuna.berlin>
2019-09-19 21:54:13 +0200

    travis test

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Cleaning maven cache"
-rm -rf $(mvn help:evaluate -Dexpression=settings.localRepository | grep -v '\[INFO\]')/*
 mvn clean --quiet
-mvn dependency:resolve-plugins --quiet
+rm -rf $(mvn help:evaluate -Dexpression=settings.localRepository | grep -v '\[INFO\]')/*
 echo "CLU cloning"
 echo "CLU installing"
 mvn --file=clu/pom.xml install -DskipTests=true --quiet
 mvn generate-resources --quiet
+mvn dependency:resolve-plugins --quiet
 git clone https://github.com/YunaBraska/command-line-util.git clu
 echo "CLU removing"
 rm -rf clu
