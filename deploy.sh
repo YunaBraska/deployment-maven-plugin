8db3436bf3f4ee7d77898f5881442b3f84f414a9
Yuna Morgenstern <io@yuna.berlin>
2019-11-02 17:13:23 +0100

    updated versions

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-"${CURRENT_DIR}"/build.sh
+"${CURRENT_DIR}"/build.sh $1
 echo "Start deploy"
 project_version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')
 mvn berlin.yuna:deployment-maven-plugin:"${project_version}":run -Djava.doc -Djava.source -Ddeploy -Dtag
